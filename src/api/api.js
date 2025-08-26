940eaa80a89a72e16c921e968d9daaad4f2391b9
midhunrobert <104688168+midhunrobert@users.noreply.github.com>
2025-08-26 22:32:17 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://wordlebackend-d520.onrender.com", // backend URL
+  baseURL: "https://wordlebackend-d520.onrender.com/api", // backend URL
 });
 
 // Attach JWT token if exists
